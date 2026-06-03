ea10ed05a86e5a0523e95a74a7d88be78ffd29b4
Dmitriy <schajckodmitrij@gmail.com>
2026-06-03 20:54:54 +0300

    docker fix

--- backend/prisma/prisma.config.ts
+++ backend/prisma/prisma.config.ts
@@ -2,10 +2,10 @@ import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-    schema: 'prisma/schema.prisma',
+    schema: 'schema.prisma',
     migrations: {
-        path: 'prisma/migrations',
-        seed: 'tsx ./prisma/seed.ts',
+        path: 'migrations',
+        seed: 'npx tsx prisma/seed.ts',
     },
     datasource: {
         url: env('DATABASE_URL'),
